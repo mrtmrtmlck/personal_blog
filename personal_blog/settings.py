d4b1439b114b62c0264a5d6d3f7510bf4017aa3f
mertmalcok <mert@mynextmatch.com>
2018-12-15 21:31:57 +0300

    Redesign navbar.

--- personal_blog/settings.py
+++ personal_blog/settings.py
@@ -130,7 +130,7 @@ AWS_QUERYSTRING_AUTH = False
 # django-storages settings
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 
