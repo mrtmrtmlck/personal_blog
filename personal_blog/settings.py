b2e84bad711174cf1e25feb3863e03dbf4ce7311
mrtmrtmlck <mertmuratmalcok@gmail.com>
2018-12-20 00:43:49 +0300

    ElasticBeanstalk deployment settings.

--- personal_blog/settings.py
+++ personal_blog/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['personal-blog-dev.eu-central-1.elasticbeanstalk.com']
 
 # Application definition
 
